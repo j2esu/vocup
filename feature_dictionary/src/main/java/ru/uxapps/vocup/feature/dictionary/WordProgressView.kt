6c08fff25deb707fd79a2e9ace3e361980676094
j2esu <ipegorovev@gmail.com>
2020-08-26 21:22:05 +0300

    Sweep gradient arc variant

--- feature_dictionary/src/main/java/ru/uxapps/vocup/feature/dictionary/WordProgressView.kt
+++ feature_dictionary/src/main/java/ru/uxapps/vocup/feature/dictionary/WordProgressView.kt
@@ -1,41 +1,58 @@
 package ru.uxapps.vocup.feature.dictionary
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
-import android.view.View.MeasureSpec.EXACTLY
 import androidx.core.content.res.use
+import androidx.core.graphics.rotationMatrix
 import androidx.core.math.MathUtils
 
 class WordProgressView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
-    private val mainPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var firstColor: Int = 0
+    private var secondColor: Int = 0
+    private var radius: Float = 0f
+    private var strokeWidth: Float = 0f
+    private var dashWidth = 3f
 
     init {
         context.obtainStyledAttributes(attrs, R.styleable.WordProgressView).use {
-            mainPaint.color = it.getColor(R.styleable.WordProgressView_mainColor, Color.BLACK)
+            firstColor = it.getColor(R.styleable.WordProgressView_firstColor, Color.BLACK)
+            secondColor = it.getColor(R.styleable.WordProgressView_secondColor, Color.BLACK)
+            radius = it.getDimension(R.styleable.WordProgressView_radius, 0f)
+            strokeWidth = it.getDimension(R.styleable.WordProgressView_strokeWidth, 0f)
         }
     }
 
-    private var progress: Int = 100
+    private val progressPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.STROKE
+        strokeWidth = this@WordProgressView.strokeWidth
+        shader = SweepGradient(radius, radius, firstColor, secondColor).apply {
+            setLocalMatrix(rotationMatrix(-90f, radius, radius))
+        }
+    }
+
+    private val dashPaint = Paint().apply {
+        style = Paint.Style.STROKE
+        color = firstColor
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = dashWidth
+    }
+
+    private var progress: Int = 60
+    private val path = Path()
+    private val size = radius * 2
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        require(MeasureSpec.getMode(widthMeasureSpec) == EXACTLY) { "Require exact width" }
-        require(MeasureSpec.getMode(heightMeasureSpec) == EXACTLY) { "Require exact height" }
-        val width = MeasureSpec.getSize(widthMeasureSpec)
-        val height = MeasureSpec.getSize(heightMeasureSpec)
-        require(width == height) { "Require square form" }
-        setMeasuredDimension(width, height)
+        setMeasuredDimension(size.toInt(), size.toInt())
     }
 
     override fun onDraw(canvas: Canvas) {
-        val w = measuredWidth.toFloat()
-        val h = measuredHeight.toFloat()
         val angle = 360f * progress / 100f
-        canvas.drawArc(0f, 0f, w, h, -90f, angle, true, mainPaint)
+        path.arcTo(0f, 0f, size, size, -90f, angle, true)
+        canvas.drawPath(path, progressPaint)
+        canvas.drawLine(radius, -strokeWidth, radius, strokeWidth, dashPaint)
     }
 
     fun setProgress(progress: Int) {
