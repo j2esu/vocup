705a5cee9dca1de5c524dbd7c0b01634601d9d54
j2esu <ipegorovev@gmail.com>
2020-08-26 13:18:03 +0300

    Fix bug with configuring soft input mode

--- app/src/main/java/ru/uxapps/vocup/MainActivity.kt
+++ app/src/main/java/ru/uxapps/vocup/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN
 import android.view.WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import androidx.fragment.app.commit
+import androidx.fragment.app.*
 import ru.uxapps.vocup.feature.worddetails.WordFragment
 import ru.uxapps.vocup.workflow.AddWordWorkflow
 
@@ -26,7 +23,10 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), NavFragment.Rout
                 override fun onFragmentStarted(fm: FragmentManager, f: Fragment) {
                     when (f) {
                         is WordFragment -> window.setSoftInputMode(SOFT_INPUT_ADJUST_PAN)
-                        else -> window.setSoftInputMode(SOFT_INPUT_ADJUST_RESIZE)
+                        !is DialogFragment -> {
+                            @Suppress("DEPRECATION")
+                            window.setSoftInputMode(SOFT_INPUT_ADJUST_RESIZE)
+                        }
                     }
                 }
             },
