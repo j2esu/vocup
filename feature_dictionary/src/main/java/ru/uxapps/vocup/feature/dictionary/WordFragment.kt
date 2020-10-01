eac324cdfdd5c37ef442a8a45f2825b974bc9603
j2esu <ipegorovev@gmail.com>
2020-10-01 10:20:00 +0300

    Cleanup long package names

--- feature_dictionary/src/main/java/ru/uxapps/vocup/feature/dictionary/WordFragment.kt
+++ feature_dictionary/src/main/java/ru/uxapps/vocup/feature/dictionary/WordFragment.kt
@@ -8,14 +8,17 @@ import androidx.lifecycle.observe
 import ru.uxapps.vocup.feature.BaseFragment
 import ru.uxapps.vocup.feature.awaitValue
 import ru.uxapps.vocup.feature.dictionary.databinding.FragmentWordBinding
+import ru.uxapps.vocup.feature.dictionary.di.WordViewModel
+import ru.uxapps.vocup.feature.dictionary.model.WordDetails
+import ru.uxapps.vocup.feature.dictionary.view.AddTransDialog
+import ru.uxapps.vocup.feature.dictionary.view.EditTransDialog
+import ru.uxapps.vocup.feature.dictionary.view.WordView
 import ru.uxapps.vocup.feature.tts
 import ru.uxapps.vocup.util.consume
 import ru.uxapps.vocup.util.host
 import javax.inject.Inject
 
-class WordFragment : BaseFragment(R.layout.fragment_word),
-    ru.uxapps.vocup.feature.dictionary.view.AddTransDialog.Host,
-    ru.uxapps.vocup.feature.dictionary.view.EditTransDialog.Host {
+class WordFragment : BaseFragment(R.layout.fragment_word), AddTransDialog.Host, EditTransDialog.Host {
 
     interface Router {
         fun onWordDeleted(undo: suspend () -> Unit)
@@ -26,34 +29,30 @@ class WordFragment : BaseFragment(R.layout.fragment_word),
         private val WordFragment.wordId get() = requireArguments()["word"] as Long
     }
 
-    private val vm by viewModels<ru.uxapps.vocup.feature.dictionary.di.WordViewModel>()
+    private val vm by viewModels<WordViewModel>()
 
-    @Inject internal lateinit var detailsModel: ru.uxapps.vocup.feature.dictionary.model.WordDetails
+    @Inject internal lateinit var detailsModel: WordDetails
 
     override fun onViewReady(view: View, init: Boolean) {
         vm.getWordComponent(wordId).inject(this)
-        val v = ru.uxapps.vocup.feature.dictionary.view.WordView(
-            FragmentWordBinding.bind(view),
-            object : ru.uxapps.vocup.feature.dictionary.view.WordView.Callback {
+        val v = WordView(FragmentWordBinding.bind(view), object : WordView.Callback {
 
-                override fun onDelete() = detailsModel.onDeleteWord()
-                override fun onAddTrans() =
-                    ru.uxapps.vocup.feature.dictionary.view.AddTransDialog().show(childFragmentManager, null)
+            override fun onDelete() = detailsModel.onDeleteWord()
+            override fun onAddTrans() =
+                AddTransDialog().show(childFragmentManager, null)
 
-                override fun onDeleteTrans(trans: String) = detailsModel.onDeleteTrans(trans)
-                override fun onReorderTrans(newTrans: List<String>) = detailsModel.onReorderTrans(newTrans)
+            override fun onDeleteTrans(trans: String) = detailsModel.onDeleteTrans(trans)
+            override fun onReorderTrans(newTrans: List<String>) = detailsModel.onReorderTrans(newTrans)
 
-                override fun onListen() {
-                    detailsModel.text.value?.let { tts.speak(it) }
-                }
+            override fun onListen() {
+                detailsModel.text.value?.let { tts.speak(it) }
+            }
 
-                override fun onEditTrans(trans: String) {
-                    ru.uxapps.vocup.feature.dictionary.view.EditTransDialog().apply {
-                        arguments = ru.uxapps.vocup.feature.dictionary.view.EditTransDialog.argsOf(trans)
-                    }
-                        .show(childFragmentManager, null)
-                }
-            })
+            override fun onEditTrans(trans: String) {
+                EditTransDialog().apply { arguments = EditTransDialog.argsOf(trans) }
+                    .show(childFragmentManager, null)
+            }
+        })
         with(detailsModel) {
             translations.observe(viewLifecycleOwner, v::setTranslations)
             text.observe(viewLifecycleOwner, v::setWordText)
